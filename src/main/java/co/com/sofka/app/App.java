1615c18ea2df5b1a3f174b2ba3119ed049f0337f
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-27 22:29:06 -0500

    Creando clase calulator y main

--- src/main/java/co/com/sofka/app/App.java
+++ src/main/java/co/com/sofka/app/App.java
@@ -1,13 +1,49 @@
 package co.com.sofka.app;
 
+import co.com.sofka.app.calculator.BasicCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger( App.class );
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+
+        System.out.println( "Enter number 1: " );
+        String textNumber1 = bufferedReader.readLine();
+
+        System.out.println( "Enter number 2: " );
+        String textNumber2 = bufferedReader.readLine();
+
+        Long number1 = Long.valueOf( textNumber1 );
+        Long number2 = Long.valueOf( textNumber2 );
+
+        BasicCalculator calculator = new BasicCalculator();
+        long result = calculator.sum( number1, number2 );
+
+        long result2 = calculator.subtraction(number1,number2);
+        long result3 = calculator.multiplication(number1,number2);
+        Float result4= calculator.division(number1,number2);
+
+
+        System.out.println( number1 + " + " + number2 + " = " + result );
+
+        System.out.println( number1 + " - " + number2 + " = " + result2 );
+
+        System.out.println( number1 + " * " + number2 + " = " + result3 );
+
+        System.out.println( number1 + " / " + number2 + " = " + result4 );
+
     }
+
 }
---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -0,0 +1,39 @@
+package co.com.sofka.app.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info("Summing {} + {}," + number1 + number2);
+        return number1 + number2;
+    }
+
+    public Long subtraction(Long number1, Long number2) {
+        logger.info("Summing {} + {}," + number1 + number2);
+        return number1 - number2;
+    }
+
+    public Long multiplication(Long number1, Long number2) {
+        logger.info("Summing {} + {}," + number1 + number2);
+
+        return number1 * number2;
+    }
+
+    public Float division(float number1, float number2) throws ArithmeticException {
+        logger.info("Summing {} + {}," + number1 +" "+ number2);
+       if(number2 ==0){
+           System.out.println("No se puede dividir por cero");
+       }
+        return number1/number2;
+    }
+
+    public static void main(String[] args) {
+        BasicCalculator operacio = new BasicCalculator();
+            Float resultado = operacio.division(1,0);
+        System.out.println(resultado);
+    }
+}
+
